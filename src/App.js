2af4b88440e8dd61790da7fd2cfad7f72022bf38
mxhs <maxwellstofman@gmail.com>
2020-10-09 12:28:14 -0700

    Finished Hooks and MVP. Making CSS better.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios'
-import CharacterListContainer from './components/Character.js'
+import CharacterContainer from './components/Character.js'
 import styled from 'styled-components'
 
 const StyledHeader = styled.div`
   h1{
     background-color: rgba(255, 255, 255, .8);
     background-blend-mode: overlay;
-    width: 18%;
-    padding: 1% 0;
+    width: 35%;
+    padding: 1.5% 1%;
     margin: 2% auto;
     border-radius: 8px;
   }
@@ -19,17 +19,13 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [characterData, setCharacterData] = useState({})
+  const [characters, setCharacters] = useState([])
 
   useEffect(() =>{
     axios.get('https://rickandmortyapi.com/api/character/')
     .then(res => {
-      const characterArr = Object.values(res.data.results)
-      
-      let infoByCharacter = characterArr.map(item =>{
-        return Object.values(item)
-      })
-      console.log('me', infoByCharacter)
+      console.log(res.data.results)
+      setCharacters(res.data.results)
     })
     .catch(err => {
       console.log(err)
@@ -43,11 +39,23 @@ const App = () => {
   return (
     <div className="App">
       <StyledHeader>
-        <h1 className="Header">Characters</h1>
+        <h1 className="Header">Rick and Morty Characters</h1>
       </StyledHeader>
-      <CharacterListContainer charData={characterData}/>
+      {
+        characters.map(char =>{
+          return <CharacterContainer key={char.id} info={char} />
+        })
+      }
     </div>
   );
 }
 
 export default App;
+
+
+// const characterArr = Object.values(res.data.results)
+      
+//       let infoByCharacter = characterArr.map(item =>{
+//         return Object.values(item)
+//       })
+//       console.log('me', infoByCharacter)
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,85 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
+const StyledCharBox = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    margin: 2% auto;
+    font-family: 'Roboto' ;
+    font-weight: 700;
+    *{
+        border: 1px solid lightgoldenrodyellow;
+    }
+
+    .charCard{
+        margin: 2% auto;
+    }
+
+    img{
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+        width: 300px;
+        height: 100%;
+        margin: 0px;
+        opacity: 1;
+        transition: opacity 0.5s ease 0s;
+        object-position: center center;
+        object-fit: cover;
+    }
+
+    p{
+        font-weight: 400;
+    }
+
+    section{
+        display: flex;
+        flex-direction: column;
+        text-align: left; 
+        margin: 2% auto;
+        /* justify-content: space-around; */
+    }
+
+    h3{
+        color: #9e9e9e;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    .charInfo{
+        background-color: rgba(60, 62, 68, .85);
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+        padding: .75rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: baseline;
+        align-items: baseline;
+    }
+`
+
 // Write your Character component here
-export default function Characters (charData){
-    
+export default function Characters (prop){
+    const {info} = prop
     return(
-        <div className='characterDiv'>
-            <img src={charData.url}/>
-            <h1>{charData.name}</h1>
-        </div>
+        <StyledCharBox className='charCard'>
+            <img src={info.image} alt=''/>
+            <div className='charInfo'>
+                <section>
+                    <h2>{info.name}</h2>
+                    <p>• {info.status} - {info.species}</p>
+                </section>
+                <section>
+                    <h3>Last Known Location:</h3>
+                    <p>{info.location.name}</p>
+                </section>
+                <section>
+                    <h3>First Seen In:</h3>
+                    <p>{info.episode[0].name}</p>
+                </section>
+            </div>
+        </StyledCharBox>
     )
 }
